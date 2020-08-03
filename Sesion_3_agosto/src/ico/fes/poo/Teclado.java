837a2fa3b8f8233d059f5f26a87ada5c714c4407
ulyseshrrs <uliseshernandez52@aragom.unam.mx>
2020-08-03 18:54:28 -0500

    Codigo de la clase del día 3 de agosto

---
+++ Sesion_3_agosto/src/ico/fes/poo/Teclado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo;
+
+/**
+ *
+ * @author uliw8
+ */
+public class Teclado {
+    int numeroTeclas;
+    
+    public void teclear(int keyCode){
+        System.out.println("Se presionó la tecla:" + keyCode);
+    }  
+}
---
+++ Sesion_3_agosto/src/sesion_3_agosto/Sesion_3_agosto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion_3_agosto;
+
+import ico.fes.poo.Teclado;
+
+/**
+ *
+ * @author uliw8
+ */
+public class Sesion_3_agosto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Teclado objTeclado1= new Teclado();
+        objTeclado1.teclear(10);
+        Teclado t2=new Teclado();
+        Teclado t3=new Teclado();
+        Teclado t4=new Teclado();
+        Teclado t5=new Teclado();
+    }
+    
+}
